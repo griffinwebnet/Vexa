702ee6afb5eab345a418e4743de053ddb92b552b
JL Griffin <jlgriffin@webnetvn.com>
2025-10-07 05:33:06 -0500

    0.3.11

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ set -e
 
 # Vexa Bootstrap Script
 echo "======================================"
-echo "  Vexa Bootstrap Installer  v0.3.110"
+echo "  Vexa Bootstrap Installer  v0.3.111"
 echo "======================================"
 echo ""
 
